5dcfb1205c3b3b0a61fb3d7e26173e9ba99c043c
goutou <1243851424@qq.com>
2021-07-15 23:24:03 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,27 @@
+# """
+# print("你好")
+# print("你好"+"世界")
+# print("世界"*100)
+# print("你好")
+# print(((100+200)/2.5)-300)
+# """
+# a = int(input("请输入："))
+# b = int(input("请输入："))
+# print(a+b)
+# print(type(2333))
+# print(type("撒点"))
+# print(type(2.3))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# c = float(100)
+# print(c)
+
+
+
+# d = "asduqwoiehjkjfywlkajsajfuwqjedadhlkadj"
+# print(len(d))
+f = input()
+g = input()
+print(len(f+g))
